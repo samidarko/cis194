c08f071e0993bdf00b4c6a39d5ce85482e6c5f93
vincent <vincent@gephen.co>
2016-05-12 17:47:42 +0800

    exercice 4

--- 01-intro.hs
+++ 01-intro.hs
@@ -15,3 +15,14 @@ doubleEveryOther x = reverse [ if snd x == True then fst x * 2 else fst x | x <-
 
 sumDigits :: [Integer] -> Integer
 sumDigits x = sum $ map (sum . toDigits) x
+
+validate :: Integer -> Bool
+validate x = f $ (sumDigits . doubleEveryOther . toDigits) x `mod` 10
+    where f y = if y == 0 then True else False
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi i p1 p2 p3 = [(p1, p2), (p2, p3)]
+
